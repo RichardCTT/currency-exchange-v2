78d9b4f8e7f767b010443113ad6f5d87eaf70a26
raul <992190862@qq.com>
2025-07-24 03:51:35 +0000

    delete bug fix

--- routes/user.js
+++ routes/user.js
@@ -2,17 +2,18 @@ import express from 'express';
 import pool from '../config/db.js'
 
 const router = express.Router();
-const conn = pool.getConnection();
+const conn = await pool.getConnection();
 
 router.delete('/:id', (req, res) => {
     const userId = req.params.id
     try {
-        const queryResult = conn.query("delete from user where id = $1", [userId]);
+        const queryResult = conn.query("delete from user where user_id = ?", [userId]);
         if (queryResult.affectRows === 0) {
             console.log("no records are affected!");
         } else {
             console.log("delete successful");
         }
+        return res.end("ok");
     }
     catch (error) {
         console.log("delete failed");
